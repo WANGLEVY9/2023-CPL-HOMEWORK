d9bbb7b6639be087eabbf629d64dfc9c830e179c
WANGLEVY9 <144114671+WANGLEVY9@users.noreply.github.com>
2023-09-17 10:05:09 +0800

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,28 @@
+//This is a single-line comment.(since C99)
+// Created by 22166 on 2023/9/6.
+//stdio.h:standard input/output
+//#include<stdio.h>
+/*
+ * #include:preprocessor directive
+ */
+//main:function
+//takes no arguments,returns an integer(int)
+//printf:print+f(format)
+//"Hello World!\n":string
+#include <stdio.h>
+/*
+ * main:function
+ * y<-f(x)
+ */
+int main(void)
+{
+    printf("Hello World\n");
+    return 0;//0:exit code
+}
+/*
+ *This is a multiple line.
+ * Don't mind this.
+ *
+ *
+ *
+ */
